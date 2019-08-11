b16731f9a74c3901bc09d0d0f624f52f5428097c
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-08-11 17:23:53 +0700

    Update NewsData

--- app/src/main/java/id/rwxds/madp_submission/NewsData.java
+++ app/src/main/java/id/rwxds/madp_submission/NewsData.java
@@ -4,8 +4,46 @@ import java.util.ArrayList;
 
 public class NewsData {
     public static String[][] data = new String[][] {
-            {"Hamtaro", "Kasian hamtaro dia sedang mencari ibunya namun meskipun dia kesusahan dia tetap tidak menyerah dan akan mencari tahu kemana ia harus menemuinya", "https://vignette.wikia.nocookie.net/hamtaro/images/1/1d/HamtaroN.png/revision/latest/scale-to-width-down/350?cb=20150928215408"},
-            {"Hachi", "Sedangkan Hachi, hachi malah mabok-mabokan dan bebruat tidak patut. Dimana seharusnya ia mencari ibunya, malah ibunya yang harus mencari dia. Ibu hachi kecewa ..", "https://vignette.wikia.nocookie.net/hamtaro/images/1/1d/HamtaroN.png/revision/latest/scale-to-width-down/350?cb=20150928215408"},
+            {
+                    "Demon Slayer: Kimetsu no Yaiba Manga Gets 2nd Novel",
+                    "Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill ...",
+                    "https://cdn.animenewsnetwork.com/thumbnails/fit400x400/cms/news.3/149909/kimetsunovel2.jpg",
+                    "The official website for Shueisha's Jump j Books imprint revealed on Friday that Koyoharu Gotouge's Demon Slayer: Kimetsu no Yaiba manga is inspiring a second novel on October 4. Aya Yajima is returning to write the second novel, titled Kimetsu no Yaiba Katahane no Chō (Demon Slayer: Kimetsu no Yaiba One-Winged Butterfly).\nThe novel will be 224 pages long with a four-page frontispiece.\n\nThe manga's anime adaptation premiered on April 6, and Aniplex of America has licensed the series. The show is streaming on Hulu, Crunchyroll, and FunimationNow. An English dub will air on Adult Swim's Toonami block.\nThe anime will have 26 episodes.\n\nThe manga inspired the two-chapter side-story manga “Kimetsu no Yaiba: Tomioka Giyū Gaiden” by Ryōji Hirano (Bozebeats manga) in April. Hirano also launched the four-panel spinoff manga Kimetsu no Aima! on Shueisha's Shonen Jump+ app and website in April.",
+                    "Anime News Network",
+                    "Jennifer Sherman - 2 days ago"
+            },
+            {
+                    "Firefighter Anime Fire Force Self-Censors in Response to Kyoto Animation Arson Attack",
+                    "Firefighter-themed sh?nen anime Fire Force has delayed and edited its episodes in response to an arson attack committed last Thursday at ...",
+                    "https://cdn.pastemagazine.com/www/articles/2019/07/26/fireforcelead.jpg",
+                    "Firefighter-themed shōnen anime Fire Force has delayed and edited its episodes in response to an arson attack committed last Thursday at Kyoto Animation Studios that ranks as one of Japanese's deadliest mass-murders since the end of WWII.\n\nFire Force revolves around a heroic supernatural fire brigade who combat mysterious demons called Infernals. These skeletal demons are born when a regular human spontaneously combusts into a ball of flame.\n\nThe series is animated at David Production, and it is easy to see how an anime regularly portraying death by fire is insensitive in light of the arson attack at an anime studio. According to a statement by the MBS channel, where Fire Force airs, the anime's forthcoming third episode has been delayed until July 26. In addition, the color of fire has been changed, presumably to a more unnatural, magical hue.\n\nThe channel didn't comment on whether further episodes of the anime will retain the color change, but did say that “from episode 4 onward, we will deal [with similar situations] accordingly.”\n\nThe fire at Kyoto Animation, the creators of beloved shows like Clannad , The Melancholy of Haruhi Suzumiya and Free, claimed the lives of 34 people and left dozens more injured.",
+                    "Paste Magazine",
+                    "Substitute Thapliyal - 2 weeks ago"
+            },
+            {
+                    "Brutal Vinland Saga Clip Proves Anime's Dark Reputation Is True",
+                    "GAAH! Vinland Saga certainly popped some eyes in one of its most recent clips, displaying just how horrific the show can be when it wants.",
+                    "https://media.comicbook.com/2019/07/3558713-viking-1180107.jpeg?auto=webp&width=692&height=389&crop=692:389,smart",
+                    "In the clip, one younger viking from Thors' village is confronted by a viking from a town that is a few mountains over. While the outsiders were brought to our protagonists' village thanks in part to an escaped slave, a battle was about to break loose thanks in part to a personal grudge. The grudge itself was stopped by the slave's master, who wraps his chain around the face of the instigator, and pulling the chain back, ends the conflict in the most horrific way possible.\n\nWith the chain returned, so to does bits of hair and skin return with it.\n\nThis gut wrenching scene goes to show how serious Vinland Saga can be, and while there have only been three episodes released onto Amazon Prime so far, the franchise is definitely worth your time so far.\n\nOriginally created by Makoto Yukimura for Kodansha's Weekly Shonen Magazine, before being moved to Afternoon, in 2005, Vinland Saga has been collected into 21 volumes and has more than five million copies in print as of 2018. The series features a dramatization of King Cnut the Great's rise to power in English at the start of the 11th century.",
+                    "Comic Book",
+                    "Evan Valentine - 3 weeks ago"
+            },
+            {
+                    "Dr. Stone Releases DIY Book Of Science Experiments",
+                    "Anime News Network shared the release information that the book, “Dr. Stone's Ultimate Independent Reasearch” had been released last week ...",
+                    "https://media.comicbook.com/2019/08/dr-stone-1182051.jpeg?auto=webp&width=696&height=397&crop=696:397,smart",
+                    "A new stone world requires new methods of technology in order to bring humanity back to where it once was. Enter Senku and Taiju, two high school best friends who attempt to use their brains and their brawns to bring things back to the modern day after being frozen in stone for thousands of years. Finding themselves freed from their prisons thanks to a bit of luck and a mysterious fluid, the pair attempts to free not only their friends, but the world at large from their fates.\n\nNow, for those interested in performing some of Senku's experiments, a book of “Do It Yourself” is coming with that in mind!\n\nAs mentioned earlier, these experiments will be fairly safe, including teaching readers how to make a telescope out of a magnifying glass, how to make balloons out of garbage bags and many other things.\n\nThe experiments that take place in the Dr. Stone series proper are pretty dangerous when all is said and done, with one of the most recent efforts being creating gun powder. Obviously we don't think that the latter will be making its way into a “DIY” book any time soon.",
+                    "Comic Book",
+                    "Evan Valentine - 5 days ago"
+            },
+            {
+                    "Violet Evergarden Gaiden Shares New Poster Confirming Original Release Date",
+                    "One of such releases is the Violet Evergarden side-story film, which has released a new poster to its official Twitter account that also confirmed ...",
+                    "https://pbs.twimg.com/media/EAexmweUEAI1LfD?format=jpg&name=medium",
+                    "Kyoto Animation was recently the victim of a terrible arson that resulted in the loss of many casualties, and fans have showered the prolific studio in support. Many fans expected for Kyoto Animation's upcoming releases to be delayed in the wake of this tragedy, and while there have been some delays of a few special events, many other projects from the studio will be releasing as scheduled.\n\nOne of such releases is the Violet Evergarden side-story film, which has released a new poster to its official Twitter account that also confirmed that it will be releasing into theaters in Japan this September as previously planned.\n\nAfter announcing they will not delay the film's upcoming world premiere, Kyoto Animation confirmed Violet Evergarden Gaiden: Eternity and the Auto Memories Doll will be opening in theaters in Japan on September 6. Originally slated to screen for two weeks in theaters, the release of the new film has been extended to three weeks and will run from September 6th through September 26th.\n\nThis new film is a supplemental story to the first season of the anime series, and while not much is known about the release outside of this new poster it has been revealed that Minori Chihara will perform the opening theme, “Amy.” This won't be the only new project coming from the Violet Evergarden anime franchise either as Violet Evergarden the Movie is currently slated for a release in January 2020.\n\nThis sequel film project will take place years after the events of the war and feature Taichi Ishidate returning from the series to direct.. Other returning staff members include Reiko Yoshida as scriptwriter, Akiko Takase as character designer and chief animation director, and Evan Call as composer.",
+                    "Comic Book",
+                    "Nick Valdez - 2 weeks ago"
+            }
     };
 
     public static ArrayList<News> getListData() {
@@ -15,6 +53,9 @@ public class NewsData {
             news.setTitle(newsData[0]);
             news.setSummary(newsData[1]);
             news.setImage(newsData[2]);
+            news.setContent(newsData[3]);
+            news.setSource(newsData[4]);
+            news.setAuthor(newsData[5]);
 
             list.add(news);
         }
