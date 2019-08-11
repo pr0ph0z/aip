1833cd1a903c88d9fb72fd2b209b95668d3fc252
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-08-11 21:15:16 +0700

    Add new activity

---
+++ app/src/main/java/id/rwxds/madp_submission/AnimangaDetailActivity.java
@@ -0,0 +1,14 @@
+package id.rwxds.madp_submission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimangaDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animanga_detail);
+    }
+}
