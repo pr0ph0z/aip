08dad14aa612dfa19eae4d5f7a99e02daca28adc
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-08-11 17:31:15 +0700

    Update news detail placeholder with real data

--- app/src/main/java/id/rwxds/madp_submission/NewsDetailActivity.java
+++ app/src/main/java/id/rwxds/madp_submission/NewsDetailActivity.java
@@ -5,16 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class NewsDetailActivity extends AppCompatActivity {
+
+    TextView newsTitle, newsSource, newsAuthor, newsContent;
+    ImageView newsImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
 
+        newsTitle = findViewById(R.id.tv_news_title);
+        newsImage = findViewById(R.id.img_news_detail);
+        newsSource = findViewById(R.id.tv_news_source);
+        newsAuthor = findViewById(R.id.tv_news_author);
+        newsContent = findViewById(R.id.tv_news_content);
+
         Intent intent = getIntent();
         News news = intent.getParcelableExtra("data");
-        String image = news.getImage();
+
+        newsTitle.setText(news.getTitle());
+        newsSource.setText(news.getSource());
+        newsAuthor.setText(news.getAuthor());
+        newsContent.setText(news.getContent());
+        Glide.with(getApplicationContext())
+                .load(news.getImage())
+                .into(newsImage);
     }
 }
