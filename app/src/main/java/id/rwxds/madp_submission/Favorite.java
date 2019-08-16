c5740dbe9e250e4b64009fcfc93b82ec769bdb30
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-08-16 23:08:28 +0700

    Update setter getter

--- app/src/main/java/id/rwxds/madp_submission/Favorite.java
+++ app/src/main/java/id/rwxds/madp_submission/Favorite.java
@@ -7,6 +7,12 @@ public class Favorite implements Parcelable {
     private String title;
     private String image;
     private String rating;
+    private String ranked;
+    private String popularity;
+    private String members;
+    private String type;
+    private String aired;
+    private String genres;
 
     public Favorite() {
         //
@@ -16,6 +22,12 @@ public class Favorite implements Parcelable {
         title = in.readString();
         image = in.readString();
         rating = in.readString();
+        ranked = in.readString();
+        popularity = in.readString();
+        members = in.readString();
+        type = in.readString();
+        aired= in.readString();
+        genres = in.readString();
     }
 
     public static final Creator<Favorite> CREATOR = new Creator<Favorite>() {
@@ -41,7 +53,7 @@ public class Favorite implements Parcelable {
     public String getImage() {
         return image;
     }
-
+g
     public void setImage(String image) {
         this.image = image;
     }
@@ -54,6 +66,54 @@ public class Favorite implements Parcelable {
         this.rating = rating;
     }
 
+    public String getRanked() {
+        return ranked;
+    }
+
+    public void setRanked(String ranked) {
+        this.ranked = ranked;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getMembers() {
+        return members;
+    }
+
+    public void setMembers(String members) {
+        this.members = members;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAired() {
+        return aired;
+    }
+
+    public void setAired(String aired) {
+        this.aired = aired;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -64,5 +124,11 @@ public class Favorite implements Parcelable {
         parcel.writeString(title);
         parcel.writeString(image);
         parcel.writeString(rating);
+        parcel.writeString(ranked);
+        parcel.writeString(popularity);
+        parcel.writeString(members);
+        parcel.writeString(type);
+        parcel.writeString(aired);
+        parcel.writeString(genres);
     }
 }
